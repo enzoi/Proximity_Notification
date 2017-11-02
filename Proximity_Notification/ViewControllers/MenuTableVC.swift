e17cf64c9aa80ddd55662ede1aa7b56bd6f2a0d8
Yeontae Kim <ytk007@gmail.com>
2017-11-02 10:55:31 -0700

    Temporary UI setting changes

--- Proximity_Notification/ViewControllers/MenuTableVC.swift
+++ Proximity_Notification/ViewControllers/MenuTableVC.swift
@@ -14,6 +14,21 @@ class MenuTableVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You selected cell #\(indexPath.row)!")
+    }
+    
+    // MARK: Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "showSearch" {
+            let tabViewControllers = segue.destination as! UITabBarController
+            let nav = tabViewControllers.viewControllers![1] as! UINavigationController
+            let destinationViewController = nav.viewControllers.first as! SearchPlantVC
+        }
+    }
 
 }
 
